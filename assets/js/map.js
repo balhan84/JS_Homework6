453cd5c7f627f770e43abb0ad068d2751819d99d
balhan84 <balamutenkoanna@gmail.com>
2024-06-11 17:31:15 +0300

    Update map

--- assets/js/map.js
+++ assets/js/map.js
@@ -17,32 +17,31 @@ console.log(ordinalNumbers.get(2));
 //Отримайте список ключів та значень окремо.
 console.log([...ordinalNumbers.keys()]);
 console.log([...ordinalNumbers.values()]);
-// *Написати функцію, яка перероблятиме текст з числами
-// "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents."
-//  на текст з порядковими числівниками за допомогою мапи вище:
-// "This year I will enter the first grade. I have two brothers. I am the third child of my parents."
 
-const phraseWithNumbers =
-  "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.";
+// // *Написати функцію, яка перероблятиме текст з числами
+// // "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents."
+// //  на текст з порядковими числівниками за допомогою мапи вище:
+// // "This year I will enter the first grade. I have two brothers. I am the third child of my parents."
 
-function changePhrase(phraseWithNumbers) {
-  //   //get array words
-  const findNumbers = phraseWithNumbers.split(" ");
-  //   //розбили масив за пробілом за допомогою split
-  console.log(findNumbers);
-  //   //translate words if words are in dictionary
-  //   debugger;
-  const ordNumbers = findNumbers.map((number) => {
-    if (ordinalNumbers.has(number === "1")) {
-      return ordinalNumbers.get(number);
-    }
-    return number;
-  });
-  console.log(ordNumbers);
-  //   //create line with ukr words
-  const phraseWithOrdNum = ordNumbers.join(" ");
-  return phraseWithOrdNum;
-}
+// const phraseWithNumbers =
+//   "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.";
 
-const phraseWithOrdNum = changePhrase(phraseWithNumbers);
-console.log(phraseWithOrdNum);
+// function changePhrase(phraseWithNumbers) {
+//   //розбили масив за пробілом за допомогою split
+//   const findNumbers = phraseWithNumbers.split(" ");
+//   console.log(findNumbers);
+//   //знайти числа ??
+//   const ordNumbers = findNumbers.map((number) => {
+//     if (ordinalNumbers.has(number === "1")) {
+//       return ordinalNumbers.get(number);
+//     }
+//     return number;
+//   });
+//   console.log(ordNumbers);
+//  // зєднати слова у речення
+//   const phraseWithOrdNum = ordNumbers.join(" ");
+//   return phraseWithOrdNum;
+// }
+
+// const phraseWithOrdNum = changePhrase(phraseWithNumbers);
+// console.log(phraseWithOrdNum);
